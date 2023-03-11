6cb50d5c19d667b8f6480c74ccffdd1284d3d868
PASSIVE <115026174+ohm0980@users.noreply.github.com>
2023-03-11 15:18:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function addToList(name) {
+    let Total = Number(document.getElementById('Total').value);
+    let Price = Number(name);
+    let ulElement = document.querySelector('#myEl');
+    let liElement = document.createElement('li');
+
+    let plus = Total + Price;
+
+    // alert(plus);
+
+    document.getElementById('Total').value = plus
+
+    liElement.textContent = name;
+
+    ulElement.appendChild(liElement);
+}
+
+
+document.getElementById('To').addEventListener('click', function(){
+    let Total = Number(document.getElementById('Total').value);
+    let Rub = Number(prompt('กรอกจำนวนเงินที่รับมา'))
+    let Del = Rub - Total ;
+    alert(`ทอนเงินเป็นจำนวน : ${Del}`);
+    window.location.reload(true)
+})
